5de5f922a1c5d6acff589b2de97fbe37c5781e74
solanobayona <est.cristian.solano@unimilitar.edu.co>
2026-05-08 12:30:22 -0500

    Configuración inicial del canvas y contexto 2D

---
+++ script.js
@@ -0,0 +1,10 @@
+// Obtener canvas
+const canvas = document.getElementById("canvas");
+
+// Obtener contexto 2D
+const ctx = canvas.getContext("2d");
+
+// Prueba inicial
+ctx.fillStyle = "red";
+
+ctx.fillRect(50,50,50,50);
