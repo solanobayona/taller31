6f86efb48899e14c3ccfbf0079e244e3025558f5
solanobayona <est.cristian.solano@unimilitar.edu.co>
2026-05-08 12:49:46 -0500

    Creación del arreglo de escenas de prueba

--- script.js
+++ script.js
@@ -7,11 +7,68 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // ======================================================
-// CONVERTIR COORDENADAS
+// VARIABLES DE RECORTE
 // ======================================================
 
-// Convierte coordenadas matemáticas
-// a coordenadas del canvas
+let xmin = 150;
+let ymin = 100;
+
+let xmax = 500;
+let ymax = 350;
+
+// ======================================================
+// ESCENAS
+// ======================================================
+
+// Casos de prueba
+const scenes = [
+
+    {
+        name:"Dentro",
+        x1:200,
+        y1:150,
+        x2:450,
+        y2:300
+    },
+
+    {
+        name:"Fuera",
+        x1:50,
+        y1:50,
+        x2:100,
+        y2:80
+    },
+
+    {
+        name:"Entrando",
+        x1:50,
+        y1:200,
+        x2:300,
+        y2:200
+    },
+
+    {
+        name:"Saliendo",
+        x1:250,
+        y1:250,
+        x2:650,
+        y2:450
+    },
+
+    {
+        name:"Atravesando",
+        x1:50,
+        y1:50,
+        x2:650,
+        y2:400
+    }
+
+];
+
+// ======================================================
+// CONVERSIÓN DE COORDENADAS
+// ======================================================
+
 function convertY(y){
 
     return canvas.height - y;
@@ -19,35 +76,28 @@ function convertY(y){
 }
 
 // ======================================================
-// DDA
+// FUNCIÓN DDA
 // ======================================================
 
 // Función personalizada para dibujar líneas
-// usando rasterización manual
 function drawLineDDA(x1,y1,x2,y2,color){
 
-    // Diferencias
     let dx = x2 - x1;
     let dy = y2 - y1;
 
-    // Cantidad de pasos
     let steps = Math.max(
         Math.abs(dx),
         Math.abs(dy)
     );
 
-    // Incrementos
     let xIncrement = dx / steps;
     let yIncrement = dy / steps;
 
-    // Punto inicial
     let x = x1;
     let y = y1;
 
-    // Color
     ctx.fillStyle = color;
 
-    // Dibujar pixel a pixel
     for(let i = 0; i <= steps; i++){
 
         ctx.fillRect(
@@ -70,13 +120,57 @@ function drawLineDDA(x1,y1,x2,y2,color){
 }
 
 // ======================================================
+// VIEWPORT
+// ======================================================
+
+function drawViewport(){
+
+    drawLineDDA(
+        xmin,
+        ymin,
+        xmax,
+        ymin,
+        "black"
+    );
+
+    drawLineDDA(
+        xmax,
+        ymin,
+        xmax,
+        ymax,
+        "black"
+    );
+
+    drawLineDDA(
+        xmax,
+        ymax,
+        xmin,
+        ymax,
+        "black"
+    );
+
+    drawLineDDA(
+        xmin,
+        ymax,
+        xmin,
+        ymin,
+        "black"
+    );
+
+}
+
+// ======================================================
 // PRUEBA
 // ======================================================
 
+drawViewport();
+
+let line = scenes[0];
+
 drawLineDDA(
-    100,
-    100,
-    400,
-    300,
+    line.x1,
+    line.y1,
+    line.x2,
+    line.y2,
     "blue"
 );
